df5c8f9778fd20f43e516b72a58ccb1b850d7df1
Ignacio Manuel Baez <ignaciomanuelbaez@hotmail.com>
2024-05-14 15:39:12 -0300

    C:/Program Files/Git/ home active

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: "never",
   integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: "es",
